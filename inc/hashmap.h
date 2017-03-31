55bb8bd81049a6512a1423d0e455a064aa1ab526
Nathanael PINEAU <npineau@student.42.fr>
2017-03-31 12:54:59 +0200

    updt

--- inc/hashmap.h
+++ inc/hashmap.h
@@ -6,7 +6,7 @@
 /*   By: npineau <npineau@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/31 12:13:08 by npineau           #+#    #+#             */
-/*   Updated: 2017/03/31 12:13:10 by npineau          ###   ########.fr       */
+/*   Updated: 2017/03/31 12:54:03 by npineau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,23 @@ typedef int	(*t_kv_copy)(void*, void*);
 typedef int	(*t_kv_equ)(void*, void*);
 typedef size_t	(*t_kv_hash)(void*, size_t);
 
+typedef struct	s_kv_ops
+{
+	t_kv_copy	kv_copy;
+	t_kv_equ	kv_equ;
+	t_kv_hash	kv_hash;
+}				t_kv_ops;
+
 typedef struct	s_hashmap
 {
 	size_t		size;
 	size_t		capacity;
 	size_t		used;
+	double		lf_limit;
 	t_kv_copy	kv_copy;
 	t_kv_equ	kv_equ;
 	t_kv_hash	kv_hash;
+	void		*kvs;
 }				t_hashmap;
 
 #endif
---
+++ src/hashmalp_new.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hashmalp_new.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: npineau <npineau@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/31 12:52:46 by npineau           #+#    #+#             */
+/*   Updated: 2017/03/31 12:52:48 by npineau          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "inc/hashmap.h"
+
+t_hashmap	hashmap_new(t_kv_ops kv_ops,
+		double lf_limit,
+		size_t capacity,
+		size_t size)
+{
+	t_hashmap	new;
+
+	return (new);
+}
